0a245440ac479ec3592ee27f414db20d88d3775f
Isaac Porter <iporter@wlapcs.org>
2017-03-22 17:46:17 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+    createCanvas(600,400);
+    background(109,109,109);
+}
+
+function draw(){
+    line(35, 80, 400, 200);
+     
+     stroke(255,255,255);
+     fill(255,0,0);
+     rect(290,100,250,250);
+    
+    stroke(0,0,0)
+    strokeweight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80)
+    
+    background(100,100,100);
+}
